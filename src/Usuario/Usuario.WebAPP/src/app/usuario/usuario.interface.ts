5c4e95bb4c3e0c98aaa4d85db4418106153d152e
Henrique Santos <33522361+henriquesan14@users.noreply.github.com>
2021-01-05 17:52:07 -0300

    Update usuario.interface.ts

--- src/Usuario/Usuario.WebAPP/src/app/usuario/usuario.interface.ts
+++ src/Usuario/Usuario.WebAPP/src/app/usuario/usuario.interface.ts
@@ -1,7 +1,8 @@
 enum Escolaridade {
-    House = 1,
-    Apartment = 2,
-    Flat = 3
+    Infantil = 1,
+    Fundamental = 2,
+    Medio = 3,
+    Superior = 4
   }
 export interface Usuario {
     id: number;
